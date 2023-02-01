d8f7bb43cd532da199ba6baaae2af99b773a4135
iykeevans <elocohi238@gmail.com>
2023-02-01 21:10:17 +0100

    lesson-1

--- src/App.vue
+++ src/App.vue
@@ -1,90 +1,19 @@
 <script>
-import WelcomeBox from "./components/WelcomeBox.vue";
-import CustomFetch from "./components/CustomFetch.vue";
-import AboutComposition from "./components/AboutComposition.vue";
+import Recap_01_02_2023 from "./components/Recap_01_02_2023.vue";
 
 export default {
   components: {
-    WelcomeBox,
-    CustomFetch,
-    AboutComposition,
+    Recap_01_02_2023,
   },
   data() {
-    return {
-      slotName: "title",
-      title: "",
-      body: "",
-    };
+    return {};
   },
-  methods: {
-    changeSlotName() {
-      if (this.slotName === "title") {
-        this.slotName = "description";
-        return;
-      }
-
-      this.slotName = "title";
-    },
-    handleClick() {
-      (this.title = "Title"), (this.body = "Body");
-    },
-  },
+  methods: {},
 };
 </script>
 
 <template>
-  <main>
-    <nav>
-      <router-link to="/">Home</router-link>
-      <router-link to="/about">About</router-link>
-      <router-link to="/posts">Posts</router-link>
-    </nav>
-
-    <aside></aside>
-
-    <section>
-      <router-view />
-    </section>
-  </main>
-
-  <!-- <about-composition /> -->
-  <!-- <welcome-box @update="handleClick">
-  </welcome-box> -->
-
-  <!-- <button @click="changeSlotName">Change Slot</button> -->
-
-  <!-- <custom-fetch url="https://jsonplaceholder.typicode.com/todos">
-    <template #context="{ isLoading, response }">
-      <div v-if="isLoading">Loading...</div>
-      <div v-else>
-        <div v-for="item in response" :key="item.id">
-          <span style="color: blue">{{ item.title }}</span
-          >:
-          <span style="color: red">{{
-            item.completed ? "Done" : "Not Done"
-          }}</span>
-        </div>
-      </div>
-    </template>
-  </custom-fetch> -->
+  <Recap_01_02_2023 />
 </template>
 
-<style scoped>
-.title {
-  font-size: 20px;
-  color: red;
-}
-
-.body {
-  font-size: 16px;
-  color: grey;
-}
-
-.card__title {
-}
-
-.router-link-active,
-.router-link-exact-active {
-  color: red;
-}
-</style>
+<style scoped></style>
---
+++ src/components/Recap_01_02_2023.vue
@@ -0,0 +1,84 @@
+<template>
+  <div>{{ name }}</div>
+
+  <input v-model="searchValue" />
+
+  <!-- <button v-on:click="changeName">Change Name</button> -->
+  <div v-for="color in filteredColors" :key="color">
+    {{ color }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Recap_01_02_2023",
+  data() {
+    return {
+      name: "Hello World",
+      colors: ["red", "blue", "green", "yellow", "pink", "black", "gold"],
+      filteredColors: [
+        "red",
+        "blue",
+        "green",
+        "yellow",
+        "pink",
+        "black",
+        "gold",
+      ],
+      searchValue: "",
+    };
+  },
+  methods: {
+    changeName() {
+      this.name = "Adebayo";
+    },
+    filterString() {
+      //   this.colors = this.colors.filter((item) =>
+      //     item.toLowerCase().includes(this.searchValue.toLowerCase())
+      //   );
+      // loop through the colors
+      // get color
+      // do check to confirm if searchvalue is in the currentColor
+
+      const newArray = [];
+
+      if (this.searchValue.length === 0) {
+        return this.colors;
+      }
+      debugger;
+      for (let index = 0; index < this.colors.length; index++) {
+        const color = this.colors[index];
+
+        // check if the color is included in the searchValue
+        let condition = color
+          .toLowerCase()
+          .includes(this.searchValue.toLowerCase());
+
+        if (condition) {
+          newArray.push(color);
+        }
+      }
+
+      return newArray;
+    },
+  },
+  watch: {
+    searchValue: {
+      handler() {
+        this.filteredColors = this.colors.filter((item) =>
+          item.toLowerCase().includes(this.searchValue.toLowerCase())
+        );
+      },
+    },
+  },
+  computed: {
+    filteredColors() {
+      return this.colors.filter((item) =>
+        item.toLowerCase().includes(this.searchValue.toLowerCase())
+      );
+    },
+  },
+};
+</script>
+
+<style></style>
