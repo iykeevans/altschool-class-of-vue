cdd01c5d5eacce511578f097f693c76db3ca612e
Olixpin <noibiabdulrasaq@gmail.com>
2023-02-06 02:06:42 +0100

    Converted Color Picker Games to Composables

---
+++ src/composables/color-picker.js
@@ -0,0 +1,19 @@
+import { ref } from 'vue'
+
+const useColorPicker = () => {
+    const message = ref('Pick a color')
+    const colors = ref(['green', 'red', 'blue', 'purple'])
+
+    const matchColor = (color) => {
+        const randomColor =
+            colors.value[Math.floor(Math.random() * colors.value.length)]
+
+        randomColor === color
+            ? (message = `You win... [answer: ${randomColor}]`)
+            : (message = `You lose... [answer: ${randomColor}]`)
+    }
+
+    return { message, colors, matchColor }
+}
+
+export default useColorPicker
