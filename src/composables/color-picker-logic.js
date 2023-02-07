9bc99d613e3034b5c0c9d801df150a4fe4717bbb
Ponmile Adebisi <ponmileadebisi@gmail.com>
2023-02-07 13:43:59 +0100

    color picker made as composables

---
+++ src/composables/color-picker-logic.js
@@ -0,0 +1,19 @@
+import { ref } from "vue";
+
+export default function ColorPickerLogic() {
+  const colors = ["green", "purple", "red", "blue"];
+  let message = ref("Pick a color...");
+
+  const matchColor = (value) => {
+    // to select a random color based on the array index
+    const randomNumber = Math.floor(Math.random() * 3) + 1; //between 1 - 4
+
+    if (colors[randomNumber] === value) {
+      message.value = `You win... [answer: ${colors[randomNumber]}]`;
+      return;
+    }
+    message.value = `You loose [answer: ${colors[randomNumber]}]`;
+  };
+
+  return { colors, message, matchColor };
+}
