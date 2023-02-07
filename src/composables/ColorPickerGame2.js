7cdf447ec744a4d36142bcf49c992147f8251142
Christian Ofoka <ofokachristian@gmail.com>
2023-02-07 20:22:31 +0000

    //colorpickergame2 implemented

---
+++ src/composables/ColorPickerGame2.js
@@ -0,0 +1,34 @@
+import { ref } from "@vue/reactivity";
+
+export default function ColorPickerGame2                                                    () {
+  const colors = ["green", "red", "blue", "purple"];
+  let message = ref("Pick a color...");
+
+  const matchColor = (value) => {
+    // do a random color based on the array index;
+    const randomNumber = Math.floor(Math.random() * colors.length); //between 1 - 4
+
+    if (colors[randomNumber] === value) {
+      message.value = `You win... [answer: ${colors[randomNumber]}]`;
+      return;
+    }
+
+    message.value = `You loose [answer: ${colors[randomNumber]}]`;
+  };
+
+  return { colors, message, matchColor };
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
