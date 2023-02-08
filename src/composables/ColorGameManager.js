7bf40c2a3f2a52de0a466408826df4645a19ba25
Tobiloba Ibraheem <ibraheemtobiloba15@gmail.com>
2023-02-08 19:48:53 +0100

    My-color-game

---
+++ src/composables/ColorGameManager.js
@@ -0,0 +1,20 @@
+import { ref, reactive } from '@vue/reactivity';
+
+export function colorManager() {
+  const colors = ['green', 'red', 'blue', 'purple'];
+  let message = ref('Pick a color...');
+
+  const matchColor = (value) => {
+    // do a random color based on the array index;
+    const randomNumber = Math.floor(Math.random() * 3) + 1; //between 1 - 4
+
+    if (colors[randomNumber] === value) {
+      message.value = `You win... [answer: ${colors[randomNumber]}]`;
+      return;
+    }
+
+    message.value = `You loose [answer: ${colors[randomNumber]}]`;
+  };
+
+  return { colors, message, matchColor };
+}
