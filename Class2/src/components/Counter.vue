f9d23e3120131279bb17c79c6906db45a4f29184
Ezeani Ikenna Evan <32799580+iykeevans@users.noreply.github.com>
2023-11-15 20:16:20 +0100

    add new class

---
+++ Class2/src/components/Counter.vue
@@ -0,0 +1,32 @@
+<template>
+  <h1 class="red">{{ count }}</h1>
+
+  <div>
+    <button @click="handleAdd">Add</button>
+    <button @click="handleRemove">Remove</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  methods: {
+    handleAdd() {
+      this.count++;
+    },
+    handleRemove() {
+      this.count--;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.red {
+  color: red;
+}
+</style>
---
+++ Class2/src/components/VueDirectives.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <div v-text="message"></div>
+    <h1 v-html="counter"></h1>
+    <div v-show="failedExam">Emeka Failed his Exam</div>
+    <div v-if="passedExam">Emeka Passed his Exam</div>
+    <button @click="handlePassOrFail">
+      {{ failedExam ? "Pass" : "Fail" }}
+    </button>
+
+    <div v-for="(book, index) in books" :key="index">
+      <span>{{ index + 1 }}</span>
+      <span>{{ book }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "Hello World",
+      counter: "<code>0</code>",
+      failedExam: true,
+      passedExam: false,
+      books: ["Book 1", "Book 2", "Book 3"],
+      booksObj: {
+        1: "Book1",
+        2: "Book2",
+        3: "Book3",
+      },
+    };
+  },
+  methods: {
+    handlePassOrFail() {
+      this.failedExam = !this.failedExam;
+      this.passedExam = !this.passedExam;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.button-wrapper {
+  display: flex;
+  gap: 10px;
+}
+</style>
