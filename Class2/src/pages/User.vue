18393d8224de558846515b150e7bf1d09df5ac48
Ezeani Ikenna Evan <32799580+iykeevans@users.noreply.github.com>
2023-12-02 20:07:55 +0100

    vue router class beginning

---
+++ Class2/src/pages/User.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>This is the user page</div>
+  <div>Dynamic route is: {{ $route.params.id }}</div>
+  <button @click="$router.push('/user/10')">Go To 10th user</button>
+</template>
+
+<script>
+export default {
+  mounted() {
+    console.log("I mounted");
+  },
+  //   watch: {
+  //     "$route.params"() {
+  //       console.log("Param changed");
+  //     },
+  //   },
+  //   created() {
+  //     this.$watch(
+  //       () => this.$route.params,
+  //       (toParams, previousParams) => {
+  //         console.log("I changed");
+  //       }
+  //     );
+  //   },
+  beforeRouteUpdate(to, from) {
+    console.log("I changed");
+  },
+};
+</script>
+
+<style scoped></style>
